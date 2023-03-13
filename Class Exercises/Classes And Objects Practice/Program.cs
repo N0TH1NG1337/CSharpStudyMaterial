8843bb1434294468956ef8578515770dd64b2ce8
N0TH1NG <87660281+N0TH1NG1337@users.noreply.github.com>
2023-03-13 18:01:32 +0200

    Update Program.cs

--- Class Exercises/Classes And Objects Practice/Program.cs
+++ Class Exercises/Classes And Objects Practice/Program.cs
@@ -11,11 +11,22 @@ namespace ClassPractice
 
         static void Main(string[] args)
         {
-            Card card1 = new Card("Black", 1337);
-            Card card2 = new Card("White", 228);
+            // Card Q
+            Card card1 = new Card("Black", 1337); // ניצור כרטיס 1
+            Card card2 = new Card("White", 228); // ניצור כרטיס 2
             
-            Console.WriteLine(card1);
+            // נדפיס כל אחד 
+            Console.WriteLine(card1); 
             Console.WriteLine(card2);
+            
+            // Button Q
+            Button B1 = new Button(228); // ניצור אובייקט חדש
+
+            B1.AddToSize(5); // נוסיף לאובייקט 5
+            Console.WriteLine(B1.GetSize()); // אין לנו פעולה מדפיסה ולכן נדפיס לבדוק רק את הגודל
+
+            Button B2 = new Button(2, 10, "white"); // ניצור עוד אובייקט
+            Console.WriteLine(B1.IsSameSize(B2)); // נדפיס את התוצאה
             
         }
     }
