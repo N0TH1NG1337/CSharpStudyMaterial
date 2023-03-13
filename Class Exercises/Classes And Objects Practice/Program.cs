1056496e8fc107468212c42b39ec56d6db8b6a4d
N0TH1NG <87660281+N0TH1NG1337@users.noreply.github.com>
2023-03-13 18:15:10 +0200

    Update Program.cs

--- Class Exercises/Classes And Objects Practice/Program.cs
+++ Class Exercises/Classes And Objects Practice/Program.cs
@@ -28,6 +28,35 @@ namespace ClassPractice
             Button B2 = new Button(2, 10, "white"); // ניצור עוד אובייקט
             Console.WriteLine(B1.IsSameSize(B2)); // נדפיס את התוצאה
             
+            // Baby Q
+            Baby b1 = new Baby("1111", "M", 65, 3.2);
+            Baby b2 = new Baby("2222", "F", 70, 2.6);
+            Baby b3 = new Baby("3333", 60, 3.5);
+
+            b1.UpdateWeight(0.3);
+            b1.AddHeight(5);
+
+            b2.UpdateWeight(0.2);
+
+            b3.AddHeight(10);
+
+
+            if (b1.IsSame(b3))
+                Console.WriteLine("***");
+
+
+            if (b2.IsSame(b3))
+                Console.WriteLine("!!!");
+            else
+                Console.WriteLine("*!!*");
+
+            /*
+              OutPut :
+
+              ***,
+              *!!*
+             
+            */
         }
     }
 }
